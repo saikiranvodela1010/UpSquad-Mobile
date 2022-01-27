5210f3f703b47a11d16108492315f2c6a5a359d4
Ravikiran Avasarala <ravikiran.avasarala@devrabbit.com>
2022-01-27 23:22:51 +0530

    Made required changes to show images swipable

--- android/app/src/main/java/com/upsquard/MainApplication.java
+++ android/app/src/main/java/com/upsquard/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.createthumbnail.CreateThumbnailPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.rnfs.RNFSPackage;
 import com.rnfs.RNFSPackage;
